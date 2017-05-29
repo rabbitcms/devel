77317ca515a6ba90cc1692af0694fffc50d6bc37
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2017-05-29 19:38:46 +0300

    Init

---
+++ src/ModuleProvider.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace RabbitCMS\Modules\Devel;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class ModuleProvider
+ *
+ * @package RabbitCMS\Modules\Devel
+ */
+class ModuleProvider extends ServiceProvider
+{
+
+}
